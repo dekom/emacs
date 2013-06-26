39ae45028f645fb9d6f56802fe59b713d0070ca0
Xing Zhou <xing@xingzhou.me>
2013-06-26 14:36:02 -0400

    Add jedi comment for python

--- py.el
+++ py.el
@@ -4,6 +4,8 @@
 ;;; Code:
 ;; Auto Complete
 (add-hook 'python-mode-hook 'jedi:setup)
+;; In order to setup jedi: make sure to have virtualenv installed and then use
+;; el-get to install jedi
 (setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
 
