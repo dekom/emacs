a12ff36ddb06116bba00f1cad094bbfd77bd0ab3
Xing Zhou <xing@xingzhou.me>
2013-06-26 13:57:52 -0400

    Add org mode configurations

---
+++ orgmode.el
@@ -0,0 +1,10 @@
+;;; package --- Setup for Org modes
+;;; Commentary:
+
+;;; Code:
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;; org.el ends here
