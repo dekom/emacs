ecd7bc914f89a7960dfbb7002e602cd38ad97d3d
Xing Zhou <contact@xingzhou.me>
2013-11-23 11:00:47 -0500

    Remove autocomplete package and use company instead

--- programming.el
+++ programming.el
@@ -4,36 +4,43 @@
 ;;; Code:
 
 ;; auto-complete
-(require 'auto-complete-config)
-(ac-config-default)
-(global-auto-complete-mode t)
-
-;; syntax checking on the fly
-(add-hook 'after-init-hook 'global-flycheck-mode)
+;; (require 'auto-complete-config)
+;; (ac-config-default)
+;; (global-auto-complete-mode t)
 
 ;; Wrap-region for punctuation
 (require 'wrap-region)
 (wrap-region-mode 1)
 
-;; Eclipse integration
-(require 'eclim)
-(global-eclim-mode)
+;; ;; Eclipse integration
+;; (require 'eclim)
+;; (global-eclim-mode)
 
-;; Autocomplete for C/C++
-(require 'auto-complete-clang-async)
+;; ;; Autocomplete for C/C++
+;; (require 'auto-complete-clang-async)
 
-(defun ac-cc-mode-setup ()
-  (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
-  (setq ac-sources '(ac-source-clang-async))
-  (ac-clang-launch-completion-process)
-)
+;; (defun ac-cc-mode-setup ()
+;;   (setq ac-clang-complete-executable "~/.emacs.d/clang-complete")
+;;   (setq ac-sources '(ac-source-clang-async))
+;;   (ac-clang-launch-completion-process)
+;; )
 
-(defun my-ac-config ()
-  (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-  (global-auto-complete-mode t))
+;; (defun my-ac-config ()
+;;   (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+;;   (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+;;   (global-auto-complete-mode t))
 
-(my-ac-config)
+;; (my-ac-config)
+
+;; syntax checking on the fly
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(require 'helm)
+(helm-mode 1)
 
 ;; evil
 ;;(require 'evil)
